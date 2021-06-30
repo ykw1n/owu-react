c4a91450e5ed31941987f4d6e5cd687dabcecd4c
terojug <oks.viktor.mail@gmail.com>
2021-06-30 08:09:50 +0300

    fix

--- src/components/user/User.js
+++
@@ -1,8 +0,0 @@
-export default function User({item, appRoot}){
-    return(
-        <div>
-            {item.id} - {item.name}
-            <button onClick={() => appRoot(item.id) }>click</button>
-        </div>
-    );
-}
